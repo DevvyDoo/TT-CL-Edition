129cd99f1612dd6976031720c976f10e34ae7e2f
Lou <thetycoon90@gmail.com>
2024-07-12 01:39:28 +0400

    start_duo and start_quad batches

---
+++ win32/start_game_alt_3.bat
@@ -0,0 +1,12 @@
+@echo off
+title Toontown Online - Game Client
+cd..
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+set TTOFF_LOGIN_TOKEN=alt3
+:launcher
+%PPYTHON_PATH% -m toontown.launcher.TTOffQuickStartLauncher
+pause
+goto :launcher
