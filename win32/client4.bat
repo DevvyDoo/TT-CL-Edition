d6242ae117804c384d8614dbdaab6cdfc281a62b
Lou <thetycoon90@gmail.com>
2024-12-22 17:19:04 +0400

    multi-toon clients yippee

---
+++ win32/client4.bat
@@ -0,0 +1,18 @@
+@echo off
+title Toontown Online - Developer Mini-Server Launcher
+cd..
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+echo Toontown Online Developer Mini-Server Launcher
+echo.
+echo NOTE: Make sure that "mini-server" is enabled in your settings.json!
+echo.
+
+
+set TTOFF_LOGIN_TOKEN=user4
+set TTOFF_GAME_SERVER=127.0.0.1
+
+%PPYTHON_PATH% -m toontown.launcher.TTOffQuickStartLauncher
+pause
