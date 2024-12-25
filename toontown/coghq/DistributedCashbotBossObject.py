51879dcf69b801bca389e37f43cfb94b3ae18f59
Lou <thetycoon90@gmail.com>
2024-12-26 03:08:13 +0400

    uhhh part 3

--- toontown/coghq/DistributedCashbotBossObject.py
+++ toontown/coghq/DistributedCashbotBossObject.py
@@ -316,8 +316,10 @@ class DistributedCashbotBossObject(DistributedSmoothNode.DistributedSmoothNode, 
         if self.state == 'Off':
             return
         
-        if (state == 'F' or state == 's' and avId != base.localAvatar.doId) and (self.state in ['LocalGrabbed', 'LocalDropped', 'Grabbed', 'Dropped']):
-            return
+        if avId != localAvatar.doId:
+            if state in ['F', 's']:
+                if self.state in ['LocalGrabbed', 'Grabbed']:
+                    return
 
         if state == 'G':
             if self.state != 'LocalDropped':
